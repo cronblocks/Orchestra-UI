43d70fe247e7de3b8a308f7e19553feb16f58e90
usama <mahmoodify@gmail.com>
2022-04-13 01:53:42 +0500

    Adding basic projects and interdependencies

---
+++ src/Core/Orchestra.Connector/AppInfo/ConnectorInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orchestra.Connector.AppInfo
+{
+    public class ConnectorInfo
+    {
+        public static readonly string Name = "Orchestra.Connector";
+        public static readonly string Version = "1.0";
+        public static readonly DateTime VersionDate = DateTime.Parse("Tuesday, April 12, 2022");
+    }
+}
---
+++ src/UI/Orchestra.ConApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using AppInfo = Orchestra.Connector.AppInfo.ConnectorInfo;
+
+Console.WriteLine($"{AppInfo.Name} (V {AppInfo.Version}, {AppInfo.VersionDate.Date.ToLongDateString()})");
+Console.WriteLine($"");
